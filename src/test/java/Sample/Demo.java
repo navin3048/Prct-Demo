7d6dd6cf13b36362218f28451123686978fd6f69
kavya <kavya@LAPTOP-Q8GFA9DS>
2023-12-28 20:59:58 +0530

    demo and demo1 committed

--- src/test/java/Sample/Demo.java
+++ src/test/java/Sample/Demo.java
@@ -8,7 +8,14 @@ public class Demo {
 	public void demo()
 	{
 		System.out.println("This is demo");
+		
+		System.out.println("Welcome");
 	}
 	
+	@Test
+	public void demo1()
+	{
+		System.out.println("This is demo1");
+	}
 
 }
